b4005d923d26e707c580c6ccb7ab3fbeb1e00746
xand <xand@xandgate.com>
2022-09-09 18:14:17 -0400

    Created HDL agnostic IDs

--- Netlist/AcnIds.hs
+++ Netlist/AcnIds.hs
@@ -5,20 +5,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Netlist.AcnIds
-    ( AcnId (..)
+    ( newAcnId#
+    , AcnId (..)
     , verbatimId#
-    , AcnIdTable (nameMap, idMap)
-    , emptyIdTable
+    , AcnIdSet (nameMap, seenIds)
+    , emptyAcnSet
     , lookupAcnId
-    , arithmeticId#
     )
   where
 
 import              Control.DeepSeq
+import              Data.Function (($), (&))
+import              Data.Hashable (Hashable (..))
 import              Data.HashMap.Strict (HashMap)
 import qualified    Data.HashMap.Strict as HashMap
+import              Data.HashSet (HashSet)
+import qualified    Data.HashSet as HashSet
 import              Data.IntMap (IntMap)
 import qualified    Data.IntMap.Strict as IntMap
+import qualified    Data.List as List
+import qualified    Data.Maybe as Maybe
 import              Data.Text (Text (..))
 import              GHC.Generics (Generic)
 import              GHC.Stack
@@ -35,10 +41,9 @@ import              GHC.Stack
 data AcnId
     = ArithmeticId
         Int             -- ^ Key of name in name database.
-        CallStack       -- ^ Origin of the identifier.
     | VerbatimId
         Text            -- ^ Exact name of the identifier.
-        (Maybe AcnId)   -- ^ Optional parsed version of this id.
+        (Maybe AcnName) -- ^ Optional parsed version of this id.
         CallStack       -- ^ Origin.
     deriving (Show, Generic, NFData)
 
@@ -47,77 +52,206 @@ data AcnId
 -- name database, and doesn't sanitize; just use the name exactly as given.
 --
 verbatimId# :: HasCallStack => Text -> AcnId
-verbatimId# name = VerbatimId name Nothing originInfo
+verbatimId# name =
+  let
+    originInfo
+        | originIsOn = callStack
+        | otherwise  = emptyCallStack
+  in
+    VerbatimId name Nothing originInfo
+
+
+-- |
+--
+--
+data AcnName
+    = AcnName
+        { originalName      :: Text
+        -- ^ Name as it appears in source with extensions removed.
+        , nameNormalized    :: Text
+        -- ^ Version of 'originalName' converted depending on desired lookup
+        -- behaviour. Avoids name collisions in generated HDL.
+        , extensions        :: [Word]
+        , nameOrigin        :: CallStack
+        -- ^ The origin of this name in the compiler.
+        }
+    deriving (Show, Generic, NFData)
+
+-- |
+--
+acnKey# :: AcnName -> (Text, [Word])
+acnKey# (AcnName _ nm exts _) = (nm, exts)
+
+instance Eq AcnName where
+    id1 == id2 = acnKey# id1 == acnKey# id2
+    id1 /= id2 = acnKey# id1 /= acnKey# id2
+
+instance Hashable AcnName where
+    hashWithSalt salt = hashWithSalt salt . hash
+
+    hash =
+      let
+        fuzz factor ext = factor * factor * ext
+        hash# nm exts = hash (nm, List.foldl' fuzz 2 exts)
+      in
+        uncurry hash# . acnKey#
 
 
 -- |
 -- Name database for assisting ACN identifier generation.
 --
-data AcnIdTable
-    = AcnIdTable
-        { nameMap  :: IntMap Text
+data AcnIdSet
+    = AcnIdSet
+        { nameMap    :: IntMap AcnName
         -- ^ Searchable map of names.
-        , idMap    :: HashMap Text AcnId
-        -- ^ Match names to identifiers for id lookup.
-        , idSupply :: Int
+        , idSupply   :: Int
         -- ^ Arithmetic id supply.
+        , seenIds    :: HashSet AcnName
+        , freshCache :: FreshCache
         }
     deriving Show
 
 -- |
 -- An initial name database.
 --
-emptyIdTable = AcnIdTable IntMap.empty HashMap.empty 0
+emptyAcnSet = AcnIdSet
+    { nameMap    = IntMap.empty
+    , idSupply   = 0
+    , seenIds    = HashSet.empty
+    , freshCache = HashMap.empty
+    }
 
 -- |
 -- Lookup an ACN identifier in a name database.
 --
-lookupAcnId :: AcnId -> IntMap Text -> Maybe Text
+lookupAcnId :: AcnId -> IntMap AcnName -> Maybe AcnName
 lookupAcnId ident names =
     case ident of
-        ArithmeticId n _ -> IntMap.lookup n names
-        VerbatimId {}    -> Nothing
+        ArithmeticId n -> IntMap.lookup n names
+        VerbatimId {}  -> Nothing
+
 
 -- |
--- Generate a new identifier, and add its associated name to a name
--- database.
+-- Generate a new identifier, and add its associated name to a name database.
 --
-arithmeticId#
+newAcnId#
     :: HasCallStack
-    => Text
-    -> AcnIdTable
-    -> (AcnIdTable, AcnId)
-arithmeticId# name (AcnIdTable nameMap idMap supply) =
-    (table', newId)
-  where
-    -- Create a new name.
-    newId = ArithmeticId supply originInfo
-    
-    -- Update the table maps.
-    nameMap' = IntMap.insert supply name nameMap
-    idMap'   = HashMap.insert name newId idMap
-      
-    table' = AcnIdTable nameMap' idMap' (supply + 1)
+    => AcnName      -- ^ 
+    -> AcnIdSet     -- ^ Working ID database.
+    -> (AcnId, AcnIdSet)
+newAcnId# nm idSet =
+  let
+    -- Record the origin of the new name.
+    nmCopy = nm {nameOrigin = originInfo} where
+        originInfo
+            | originIsOn = callStack
+            | otherwise  = emptyCallStack
 
+    -- Check for this name in the set of already seen IDs. If it's already
+    -- been seen, extend it and try again. Otherwise, return as-is.
+    newNm
+        | nm `HashSet.member` seenIds idSet
+        = nmCopy {extensions = 0 : extensions nm}
+
+        | otherwise
+        = nmCopy
+
+    -- Check the most recent top extension for this name in the fresh cache.
+    fresh = freshCache idSet
     
+    newNm' = case lookupFreshCache fresh newNm of
+        Just currentMax ->
+          let
+            oldExts = extensions newNm
+            newExts = currentMax + 1 : tail oldExts
+          in
+            newNm {extensions = newExts}
+
+        Nothing -> newNm
+
+    fresh' = updateFreshCache fresh newNm'
+        
+    -- Get the next key, and update the name database.
+    newKey = idSupply idSet
+
+    names' = nameMap idSet & IntMap.insert newKey newNm'
+    seen'  = seenIds idSet & HashSet.insert newNm'
+
+    idSet' = idSet
+        { nameMap    = names'
+        , idSupply   = idSupply idSet + 1
+        , seenIds    = seen'
+        , freshCache = fresh'
+        }
+  in
+    (ArithmeticId newKey, idSet')
+
+
+type FreshCache = HashMap Text (IntMap Word)
+
+lookupFreshCache :: FreshCache -> AcnName -> Maybe Word
+lookupFreshCache cache nm = do
+    section <- HashMap.lookup (nameNormalized nm) cache
+    IntMap.lookup (length $ extensions nm) section
+
+updateFreshCache :: FreshCache -> AcnName -> FreshCache
+updateFreshCache cache nm =
+  let
+    nm'  = nameNormalized nm
+    exts = extensions nm
+
+    topExt = Maybe.fromMaybe 0 $ Maybe.listToMaybe exts
+
+    alter f e = Just . f . Maybe.fromMaybe e
+    
+    go0 f = HashMap.alter (alter f mempty) nm' cache
+    go1 f = IntMap.alter (alter f 0) (length exts)
+  in
+    go0 (go1 (max topExt))
+
+
+newAcnName#
+    :: HasCallStack
+    => (Text -> AcnName)
+    -> Text
+    -> AcnIdSet
+    -> (AcnId, AcnIdSet)
+newAcnName# normalizer nm idSet =
+    newAcnId# (normalizer nm) idSet
+
+suffix#
+    :: HasCallStack
+    => (Text -> AcnName)
+    -> AcnName
+    -> Text
+    -> AcnIdSet
+    -> (AcnId, AcnIdSet)
+suffix# normalizer nm0 affix idSet =
+  let
+    nm1 = normalizer $ originalName nm0 <> "_" <> affix
+    nm2 = nm1 { extensions = extensions nm1 <> extensions nm0 }
+  in
+    newAcnId# nm2 idSet
+
 -- TODO:
---  * [ ] makeBasic
---  * [ ] suffix
+--  * [x] makeBasic
+--        newAcnName#
+--
+--  * [x] suffix
+--        suffix#
+--
 --  * [ ] toText
---  * [ ] next
+--
+--
+--  * [x] next
+--
+--
 --  * [x] unsafeFromCoreId
+--        verbatimId#
+--
     
 
 -- FUNNY DEBUG THINGS
-
--- |
--- Get the origin information of an identifier from GHC.
--- 
-originInfo
-    | originIsOn = callStack 
-    | otherwise  = emptyCallStack
-
-{-# INLINE originInfo #-}
 
 -- |
 -- Should we trace the origins of identifiers?
